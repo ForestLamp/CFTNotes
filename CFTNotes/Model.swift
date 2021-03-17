74350deb8e5cd0f64d07bd0a532e7f153160d940
Alex <y100b@ya.ru>
2021-03-17 13:11:06 +0500

    1  New tableView 2 New ViewController 3 Interface

---
+++ CFTNotes/Model.swift
@@ -0,0 +1,16 @@
+//
+//  Model.swift
+//  CFTNotes
+//
+//  Created by a_sid on 17.03.2021.
+//
+
+import Foundation
+
+
+struct Notes {
+    var shortPreview: String
+    var fullPreview: String
+    var image: String
+    
+}
