d5feb9871dba6ce972c1ca2a759d78f275d6a3b3
Alex <y100b@ya.ru>
2021-03-18 22:26:42 +0500

    1 Fix from deleteAction with IOS 13

--- CFTNotes/Controllers/TableViewController.swift
+++ CFTNotes/Controllers/TableViewController.swift
@@ -38,13 +38,18 @@ class TableViewController: UITableViewController {
     
     // MARK: Table View Delegate
     
-    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
         let note = notes[indexPath.row]
-        let deleteAction = UITableViewRowAction(style: .default, title: "Delete") { (_, _) in
+        
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { _, _, complete in
             StorageManager.deleteObject(note)
             tableView.deleteRows(at: [indexPath], with: .automatic)
+            complete(true)
         }
-        return [deleteAction]
+        let configuration = UISwipeActionsConfiguration(actions: [deleteAction])
+               configuration.performsFirstActionWithFullSwipe = true
+               return configuration
     }
 
 
